efea934a6cdb81fbb3d5c1ca5f36129e32fc2bce
Hester <h.steyn@sanbi.org.za>
2022-06-24 17:32:00 +0200

    dd, dms is working

--- Converter_HS.py
+++ Converter_HS.py
@@ -3,16 +3,17 @@ import math
 # from toCoordinateFormat import *
 
 # Coordinates pattern matching regex
+#Decimal degrees
 dd_re = "(NORTH|SOUTH|[NS])?[\s]*([+-]?[0-8]?[0-9](?:[\.,]\d{3,}))([•º°]?)[\s]*(NORTH|SOUTH|[NS])?[\s]*[,/;]?[\s]*(EAST|WEST|[EW])?[\s]*([+-]?[0-1]?[0-9]?[0-9](?:[\.,]\d{3,}))([•º°]?)[\s]*(EAST|WEST|[EW])?"
 
 # degrees minutes seconds with '.' as separator - gives array with 15 values
-dms_periods = "/(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(NORTH|SOUTH|[NS])?(?:[\ \t]*[,/;][\ \t]*|[\ \t]*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(EAST|WEST|[EW])?/i"
-
+#dms_periods = "(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(NORTH|SOUTH|[NS])?(?:[\ \t]*[,/;][\ \t]*|[\ \t]*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(EAST|WEST|[EW])?"
+dms_periods = "(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?\s*(NORTH|SOUTH|[NS])?(?:[\ \t]*[,/;][\ \t]*|[\ \t]*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*(\.)[\ \t]*([0-5]?[0-9])[\ \t]*(\.)?[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?\s*(EAST|WEST|[EW])?"
 # degrees minutes seconds with words 'degrees, minutes, seconds' as separators (needed because the s of seconds messes with the S of SOUTH) - gives array of 17 values
-dms_abbr = "/(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*(D(?:EG)?(?:REES)?)[\ \t]*([0-5]?[0-9])[\ \t]*(M(?:IN)?(?:UTES)?)[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(S(?:EC)?(?:ONDS)?)?[\ \t]*(NORTH|SOUTH|[NS])?(?:[\ \t]*[,/;][\ \t]*|[\ \t]*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*(D(?:EG)?(?:REES)?)[\ \t]*([0-5]?[0-9])[\ \t]*(M(?:IN)?(?:UTES)?)[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(S(?:EC)?(?:ONDS)?)[\ \t]*(EAST|WEST|[EW])?/i"
+dms_abbr = "(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*(D(?:EG)?(?:REES)?)[\ \t]*([0-5]?[0-9])[\ \t]*(M(?:IN)?(?:UTES)?)[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(S(?:EC)?(?:ONDS)?)?[\ \t]*(NORTH|SOUTH|[NS])?(?:[\ \t]*[,/;][\ \t]*|[\ \t]*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*(D(?:EG)?(?:REES)?)[\ \t]*([0-5]?[0-9])[\ \t]*(M(?:IN)?(?:UTES)?)[\ \t]*((?:[0-5]?[0-9])(?:\.\d{1,3})?)?(S(?:EC)?(?:ONDS)?)[\ \t]*(EAST|WEST|[EW])?"
 
 #everything else - gives array of 17 values; ADDED BACK SLASHES FOR INVERTED COMMAS!
-coords_other = "/(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*([•º°\.:]|D(?:EG)?(?:REES)?)?[\ \t]*,?([0-5]?[0-9](?:\.\d{1,})?)?[\ \t]*(['′´’\.:]|M(?:IN)?(?:UTES)?)?[\ \t]*,?((?:[0-5]?[0-9])(?:\.\d{1,3})?)?[\ \t]*(''|′′|’’|´´|[\"″”\.])?[\ \t]*(NORTH|SOUTH|[NS])?(?:\s*[,/;]\s*|\s*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*([•º°\.:]|D(?:EG)?(?:REES)?)?[\ \t]*,?([0-5]?[0-9](?:\.\d{1,})?)?[\ \t]*(['′´’\.:]|M(?:IN)?(?:UTES)?)?[\ \t]*,?((?:[0-5]?[0-9])(?:\.\d{1,3})?)?[\ \t]*(''|′′|´´|’’|[\"″”\.])?[\ \t]*(EAST|WEST|[EW])?/i"
+coords_other = "(NORTH|SOUTH|[NS])?[\ \t]*([+-]?[0-8]?[0-9])[\ \t]*([•º°\.:]|D(?:EG)?(?:REES)?)?[\ \t]*,?([0-5]?[0-9](?:\.\d{1,})?)?[\ \t]*(['′´’\.:]|M(?:IN)?(?:UTES)?)?[\ \t]*,?((?:[0-5]?[0-9])(?:\.\d{1,3})?)?[\ \t]*(''|′′|’’|´´|[\"″”\.])?[\ \t]*(NORTH|SOUTH|[NS])?(?:\s*[,/;]\s*|\s*)(EAST|WEST|[EW])?[\ \t]*([+-]?[0-1]?[0-9]?[0-9])[\ \t]*([•º°\.:]|D(?:EG)?(?:REES)?)?[\ \t]*,?([0-5]?[0-9](?:\.\d{1,})?)?[\ \t]*(['′´’\.:]|M(?:IN)?(?:UTES)?)?[\ \t]*,?((?:[0-5]?[0-9])(?:\.\d{1,3})?)?[\ \t]*(''|′′|´´|’’|[\"″”\.])?[\ \t]*(EAST|WEST|[EW])?"
 
 # Function for converting coordinates in a variety of formats to decimal coordinates
 # @param {string} coordsString The coordinates string to convert
@@ -31,34 +32,35 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
     ddLng = None
     latdir = ""
     lngdir = ""
-    match = []
+    match = None
     matchSuccess = False
     
     if re.search(dd_re, coordsString, re.I): # .search() returns only first match
         match = re.match(dd_re, coordsString, re.I) 
         matchSuccess = checkMatch(match)
         
-        if matchSuccess:
-            ddLat = match[2]
-            ddLng = match[6]
-            
+        if matchSuccess == True:
+            ddLat = match.group(2)
+            ddLng = match.group(6)
+                        
             # need to fix if there are ','s instead of '.'
             if ',' in ddLat:
                 ddLat = ddLat.replace(',','.')
-            
+
             if ',' in ddLng:
                 ddLng = ddLng.replace(',', '.')
-            
+                
             # get directions
-            if match[1]:
-                latdir = match[1]
-                lngdir = match[5]
-            elif match[4]:
-                latdir = match[4]
-                lngdir = match[8]
+            if match.group(1):
+                latdir = match.group(1)
+                lngdir = match.group(5)
+            elif match.group(4):
+                latdir = match.group(4)
+                lngdir = match.group(8)
         else:
-            raise TypeError("invalid decimal coordinate format") # correct error type? Should it be ValueError?
+            raise Exception("invalid decimal coordinate format") # correct error type? Should it be ValueError?
     
+
     elif re.search(dms_periods, coordsString, re.I):
         match = re.match(dms_periods, coordsString, re.I)
         matchSuccess = checkMatch(match)
@@ -66,17 +68,17 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
         if matchSuccess:
             ddLat = abs(int(match.group(2)))
             if match[4]:
-                ddLat += match[4]/60
+                ddLat += int(match[4])/60
             if match[6]:
-                ddLat += match[6]/3600
+                ddLat += float(match[6])/3600
             if int(match.group(2)) < 0:
                 ddLat = -1 * ddLat
-                ddLng = abs(int(match[9]).group())
+            ddLng = abs(int(match[9]))
             if match[11]:
-                ddLng += match[11]/60
+                ddLng += int(match[11])/60
             if match[13]:
-                ddLng += match[13]/3600
-            if int(match[9]).group() < 0:
+                ddLng += float(match[13])/3600
+            if int(match[9]) < 0:
                 ddLng = -1 * ddLng
             
             if match[1]:
@@ -86,36 +88,38 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
                 latdir = match[7]
                 lngdir = match[14]
         else:
-            raise TypeError("invalid DMS coordinates format") # correct error type? Should it be ValueError?
+            raise Exception("invalid DMS coordinates format") # correct error type? Should it be ValueError?
     
+
     elif re.search(dms_abbr, coordsString, re.I):
         match = re.match(dms_abbr, coordsString, re.I)
         matchSuccess = checkMatch(match)
         
         if matchSuccess:
-            ddLat = abs(int(match[2]).group())
+            ddLat = abs(int(match.group(2)))
             if match[4]:
-                ddLat += match[4]/60
+                ddLat += int(match[4])/60
                 if not match[3]:
                     match[3] = ' '
             if match[6]:
-                ddLat += match[6]/3600
+                ddLat += float(match[6])/3600
                 if not match[5]:
                     match[5] = ' '
-            if int(re.search(r'\d+', match[2]).group()) < 0:
+            if int(match.group(2)) < 0:
                 ddLat = -1 * ddLat
-                ddLng = abs(int(re.search(r'\d+', match[10]).group()))
+            
+            ddLng = abs(int(match.group(10)))
             if match[12]:
-                ddLng += match[12]/60
+                ddLng += int(match[12])/60
                 if not match[11]:
                     match[11] = ' '
             if match[14]:
-                ddLng += match[14]/3600
+                ddLng += float(match[14])/3600
                 if not match[13]:
                     match[13] = ' '
-            if int(re.search(r'\d+', match[10]).group()) < 0:
+            if int(match.group(10)) < 0:
                 ddLng = -1 * ddLng
-                
+                            
             if match[1]:
                 latdir = match[1]
                 lngdir = match[9]
@@ -123,36 +127,39 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
                 latdir = match[8]
                 lngdir = match[16]
         else:
-            raise TypeError("invalid DMS coordinates format") # correct error type? Should it be ValueError?
+            raise Exception("invalid DMS coordinates format") # correct error type? 
     
+
     elif re.search(coords_other, coordsString, re.I):
         match = re.match(coords_other, coordsString, re.I)
         matchSuccess = checkMatch(match)
         
         if matchSuccess:
-            ddLat = abs(int(re.search(r'\d+', match[2]).group()))
+            ddLat = abs(int(match.group(2))) # Error "'re.Match' object does not support item assignment"
+
             if match[4]:
-                ddLat += match[4]/60
+                ddLat += int(match[4])/60
                 if not match[3]:
                     match[3] = ' '
             if match[6]:
-                ddLat += match[6]/3600
+                ddLat += float(match[6])/3600
                 if not match[5]:
                     match[5] = ' '
-            if int(re.search(r'\d+', match[2]).group()) < 0:
-                ddLat = -1 * ddLat
-                ddLng = abs(int(re.search(r'\d+', match[10]).group()))
+            if int(match.group(2)) < 0:
+                    ddLat = -1 * ddLat
+            
+            ddLng = abs(int(match.group(10)))
             if match[12]:
-                ddLng += match[12]/60
+                ddLng += int(match[12])/60
                 if not match[11]:
                     match[11] = ' '
             if match[14]:
-                ddLng += match[14]/3600
+                ddLng += float(match[14])/3600
                 if not match[13]:
                     match[13] = ' '
-            if int(re.search(r'\d+', match[10]).group()) < 0:
-                ddLng = -1 * ddLng
-            
+            if int(match.group(10)) < 0:
+                    ddLat = -1 * ddLng
+                       
             if match[1]:
                 latdir = match[1]
                 lngdir = match[9]
@@ -160,21 +167,23 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
                 latdir = match[8]
                 lngdir = match[16]
         else:
-            raise TypeError("invalid coordinates format") # correct error type? Should it be ValueError?
+            raise Exception("invalid coordinates format") # correct error type? 
     
     # check longitude value - it can be wrong!
-    if abs(ddLng) >= 180:
-        raise ValueError("invalid longitude value")
-    
+    if abs(float(ddLng)) >= 180:
+        raise Exception("invalid longitude value")
+
+    ddLat = float(ddLat)
+    ddLng = float(ddLng)
     if matchSuccess:
         # make sure the signs and cardinal directions match
-        patt = "/S|SOUTH/i"
-        if patt.search(latdir):
+        patt = "(S|SOUTH)"
+        if re.search(patt, latdir, re.I):
             if ddLat > 0:
                 ddLat = -1 * ddLat
             
-        patt = "/W|WEST/i;"
-        if patt.search(lngdir):
+        patt = "(W|WEST)"
+        if re.search(patt, lngdir, re.I):
             if ddLng > 0:
                 ddLng = -1 * ddLng
         
@@ -182,73 +191,74 @@ def convert(coordsString, decimalPlaces=5): #why use convert instead of converte
         # we can't split down the middle because if there are decimals they may have different numbers on each side
         # so we need to find the separating character, or if none, use the match values to split down the middle
 
-        verbatimCoordinates = match[0].strip()
-        verbatimLat = None
-        verbatimLng = None
-        sepChars = "/[,/;\u0020]/g" #comma, forward slash and spacebar
-        seps = verbatimCoordinates.match(sepChars)
+        # verbatimCoordinates = match[0].strip()
+        # verbatimLat = None
+        # verbatimLng = None
+        # sepChars = "/[,/;\u0020]/g" #comma, forward slash and spacebar
+        # seps = verbatimCoordinates.match(sepChars)
 
-        if seps == None:
-            # split down the middle
-            middle = len(coordsString)/2
-            verbatimLat = verbatimCoordinates[0:middle].strip()
-            verbatimLng = verbatimCoordinates[middle].strip()
-        else:          # if length is odd then find the index of the middle value
-            #get the middle index
-            middle = None
-            #easy for odd numbers
-            if (len(seps) % 2) == 1:
-                middle = len(seps)/2
-            else:
-                middle = (len(seps)/2) -1
+        # if seps == None:
+        #     # split down the middle
+        #     middle = len(coordsString)/2
+        #     verbatimLat = verbatimCoordinates[0:middle].strip()
+        #     verbatimLng = verbatimCoordinates[middle].strip()
+        # else:          # if length is odd then find the index of the middle value
+        #     #get the middle index
+        #     middle = None
+        #     #easy for odd numbers
+        #     if (len(seps) % 2) == 1:
+        #         middle = len(seps)/2
+        #     else:
+        #         middle = (len(seps)/2) -1
             
-            # walk through seps until we get to the middle
-            splitIndex = 0
+        #     # walk through seps until we get to the middle
+        #     splitIndex = 0
 
-            # it might be only one value
-            if middle == 0:
-                splitIndex = verbatimCoordinates.index(seps[0])
-                verbatimLat = verbatimCoordinates[0:splitIndex].strip()
-                verbatimLng = verbatimCoordinates[splitIndex + 1].strip()
-            else:
-                currSepIndex = 0
-                startSearchIndex = 0
-                while currSepIndex <= middle:
-                    splitIndex = verbatimCoordinates.index(seps[currSepIndex], startSearchIndex)
-                    startSearchIndex = splitIndex + 1
-                    currSepIndex += 1
+        #     # it might be only one value
+        #     if middle == 0:
+        #         splitIndex = verbatimCoordinates.index(seps[0])
+        #         verbatimLat = verbatimCoordinates[0:splitIndex].strip()
+        #         verbatimLng = verbatimCoordinates[splitIndex + 1].strip()
+        #     else:
+        #         currSepIndex = 0
+        #         startSearchIndex = 0
+        #         while currSepIndex <= middle:
+        #             splitIndex = verbatimCoordinates.index(seps[currSepIndex], startSearchIndex)
+        #             startSearchIndex = splitIndex + 1
+        #             currSepIndex += 1
                 
-                verbatimLat = verbatimCoordinates[0:splitIndex].strip()
-                verbatimLng = verbatimCoordinates[splitIndex + 1].strip()
+        #         verbatimLat = verbatimCoordinates[0:splitIndex].strip()
+        #         verbatimLng = verbatimCoordinates[splitIndex + 1].strip()
         
         # All done!!
         # just truncate the decimals appropriately
         if math.isnan(ddLat) and ',' in ddLat:
             ddLat = ddLat.replace(',', '.')
         
-        ddLat = int(int(ddLat).toFixed(decimalPlaces))
+        ddLat = round(ddLat, decimalPlaces)
 
         if math.isnan(ddLng) and ',' in ddLng:
             ddLng = ddLng.replace(',', '.')
 
-        ddLng = int(int(ddLng).toFixed(decimalPlaces))
+        ddLng = round(ddLng, decimalPlaces)
 
-    
-        # FIGURE OUT HOW TO DO RETURN HERE!!!!!!!!!!!! 
+        return [ddLat,ddLng]  
+        
     else:
-        raise ValueError("coordinates pattern match failed") #correct error type?
+        raise Exception("coordinates pattern match failed") #correct error type?
 
 def checkMatch(match): #test if the matched groups arrays are 'balanced'. match is the resulting array
-    if not math.isnan(match.group(1)): #we've matched a number, not what we want..
-        return False
-    
-    #first remove the empty values from the array
-    filteredMatch = [x for x in match if x != ""] #CHECK THIS
+        
+    groups = match.groups()
+    filteredMatch = []
+    for item in groups:
+        if item is None or item.strip() == "":
+            continue
+        else:
+            filteredMatch.append(item.strip())  
 
-    #we need to shift the array because it contains the whole coordinates string in the first item
-    newFilteredMatch = filteredMatch.pop(0)
-
-    
+                    
+          
     #if minutes or seconds are out of bounds, the array length is wrong
     # if (filteredMatch.length == 4) {
     #     return false
@@ -259,17 +269,18 @@ def checkMatch(match): #test if the matched groups arrays are 'balanced'. match 
         return False
 
     #regex for testing corresponding values match
-    numerictest = "/^[-+]?(\d+|\d+\.\d*|\d*\.\d+)$/" #for testing numeric values
-    stringtest = "/[A-Za-z]+/" #strings - the contents of strings are already matched when this is used
-
-    halflen = len(newFilteredMatch)/2
-    result = True
-    for i in range(0, halflen+1):
-        if numerictest.search(newFilteredMatch[i]) != numerictest.search(newFilteredMatch[i+halflen]) or stringtest.search(newFilteredMatch[i] != stringtest.search(newFilteredMatch[i+halflen])):
-            result = False
-            break
+    numerictest = "^[-+]?\d+([\.,]{1}\d+)$" #for testing numeric values
+   
+    halflen = int(len(filteredMatch)/2)
     
-    return result
+    for i in range(0, halflen+1):
+        leftside = filteredMatch[i]
+        rightside = filteredMatch[i + halflen]
+        if ((re.match(numerictest, leftside) and re.match(numerictest, rightside)) or (type(leftside)== str and type(rightside) == str) or leftside == rightside):
+            return True
+           
+        else:
+            return False
 
 # functions for coordinate validation
 
@@ -287,22 +298,13 @@ def coordsCloseEnough(coordsToTest):
     if ',' in coordsToTest:
         coords = coordsToTest.split(',')
         if math.isnan(int(coords[0])) == True or math.isnan(int(coords[1])) == True:
-            raise ValueError("coords are not valid decimals") # check is error type is correct
+            raise Exception("coords are not valid decimals") # check is error type is correct
         # else:
             # return decimalsCloseEnough(this.decimalLatitude, int(coords[0])) and decimalsCloseEnough(this.decimalLongitude, coords[1]) # this here will be the converted coordinates object
             # CHECK HOW TO DO .this IN PYTHON
     else:
-        raise ValueError("coords being tested must be separated by a comma") # check if error type is correct
+        raise Exception("coords being tested must be separated by a comma") # check if error type is correct
 
 
 
 
-# CONVERT BELOW TO PYTHON
-# to = Object.freeze({
-#   DMS: 'DMS',
-#   DM: 'DM'
-# })
-
-# converter.to = to
-
-# module.exports = converter
--- testindividual.py
+++ testindividual.py
@@ -1,5 +1,13 @@
 from Converter_HS import convert
-test = '27,71372° S 23,07771° E'
+#test = '-27.45.34 S 23.23.23 E'
+#test = '-25deg 10min 6.95sec S 26deg 58min 55.80sec E'
+
+#test = '24 05.346S 028 03.289E'
+#test = '22 34 20.55 S 17 05 41.05 E'
+
+# test = '   27.45637° S  23.123445° E'
+# test = '-25.5589167 31.0113888'
+test = '-27 45 34 S 23 23 23 E'
 
 #converted = convert(test)
 
