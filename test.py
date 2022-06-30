7dd993eb2dfbc5e1f61cc1ca69305a532a4c5b8e
Ian Engelbrecht <ianicus.za@gmail.com>
2022-06-30 20:17:07 +0200

    cleaned up test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import json
 
 with open("testFormats.json", encoding='utf-8') as file:
     test = json.load(file)
-    print(test)
 
 allPassed = True
 
@@ -12,11 +11,14 @@ for dict in test:
         converted = convert(dict.get("verbatimCoordinates"))
         #print(converted)
         correctlyConverted = coordsCloseEnough(converted[0],converted[1],dict.get("decimalLatitude"),dict.get("decimalLongitude"))
-        if correctlyConverted:
-            print("Conversion was correct")
-        else:
-            print(str(dict) + str(converted[0]) + ' ' + str(converted[1]) +" " + "Conversion was incorrect")
+        if not correctlyConverted:
+            print(str(dict) + str(converted[0]) + ' ' + str(converted[1]) +" " + "Conversion was incorrect") 
+            allPassed = False        
     except Exception as e:
-        print(e)
+        print('converter threw exception for', dict.get("verbatimCoordinates"))
+        allPassed = False
+
+if(allPassed):
+    print('All coordinates converted successfully')
 
 
